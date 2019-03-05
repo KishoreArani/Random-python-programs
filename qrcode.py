e378a7f37d8c463137a0b8da24e79ec4e84962fb
Kishore Arani <43106557+KishoreArani@users.noreply.github.com>
2019-03-06 01:01:07 +0530

    Update qrcode.py

--- qrcode.py
+++ qrcode.py
@@ -1,10 +1,18 @@
+'''
+Python program to generate QR Code using pyqrcode module.
+'''
+
 import pyqrcode
 from pyqrcode import QRCode
 
-
+#String which represent the QR Code
 s = "Hello World!!!"
 
+#Generate QR Code
 url = pyqrcode.create(s)
 
+#save using .svg extension
 url.svg("myqr.svg",scale = 8)
 
+#check out myqr.svg
+#Scan the QR Code and it will display "Hello World!!!"
